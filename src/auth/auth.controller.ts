171b91b7d1c4ff0b886626c3f6a17f97019b1ddf
lucas <lucas.lopes@conpec.com.br>
2020-08-09 23:32:46 -0300

    Configured passport authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,42 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { UserService } from 'src/shared/user/user.service';
 import { RegisterDTO, LoginDTO } from './auth.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  tempAuth() {
+    return { auth: 'works' };
+  }
 
   @Post('login')
   async login(@Body() UserDTO: LoginDTO) {
-    return await this.userService.findByLogin(UserDTO);
+    const user = await this.userService.findByLogin(UserDTO);
+    const payload = {
+      username: user.username,
+      seller: user.seller,
+    };
+
+    const token = await this.authService.signPayload(payload);
+    return { user, token };
   }
 
   @Post('register')
   async register(@Body() UserDTO: RegisterDTO) {
-    return await this.userService.create(UserDTO);
+    const user = await this.userService.create(UserDTO);
+    const payload = {
+      username: user.username,
+      seller: user.seller,
+    };
+    const token = this.authService.signPayload(payload);
+    return { user, token };
   }
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,18 +1,18 @@
 import { Document } from 'mongoose';
 
 interface Address {
-    addr1: string;
-    addr2: string;
-    city: string;
-    state: string;
-    country: string;
-    zip: number;
+  addr1: string;
+  addr2: string;
+  city: string;
+  state: string;
+  country: string;
+  zip: number;
 }
 
 export interface User extends Document {
-    name: string;
-    readonly password: string;
-    seller: boolean;
-    address: Address;
-    created: Date;
+  username: string;
+  readonly password: string;
+  seller: boolean;
+  address: Address;
+  created: Date;
 }
